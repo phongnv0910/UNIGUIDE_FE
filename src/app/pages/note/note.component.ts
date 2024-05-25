f7645b0a12ffd3c7ca08bd58e6cd9c79d5ac3016
tranhaaaaaa <cauchunso2002@gmail.com>
2024-05-26 00:45:19 +0700

    update note

--- src/app/pages/note/note.component.ts
+++ src/app/pages/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SignalRService } from 'src/core/services/signalr.service';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -9,26 +9,17 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit, OnDestroy {
-  noteContent: string;
+  noteContent: string = '';
   private fileId =  this.route.snapshot.paramMap.get('noteId');
   private routeSub: Subscription;
   private timeoutId: any;
   private isConnected: boolean = false;
-  @ViewChild('quillEditor') quillEditor: any;
-  editorConfig = {
-    toolbar: [
-      ['bold', 'italic', 'underline'],
-      [{ 'header': 1 }, { 'header': 2 }],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-      ['clean']
-    ]
-  };
+
   constructor(
     private signalRService: SignalRService,
     private route: ActivatedRoute
   ) {}
+
   ngOnInit() {
     console.log("fileid",this.fileId);
     debugger;
@@ -37,7 +28,9 @@ export class NoteComponent implements OnInit, OnDestroy {
       if (this.fileId) {
         this.signalRService.startConnection(this.fileId).then(() => {
           this.isConnected = true;
+          console.log("connect succes")
           this.signalRService.listenForMessages((message: string) => {
+            console.log("message",message);
             this.noteContent = message;
           });
         }).catch(error => {
@@ -46,34 +39,26 @@ export class NoteComponent implements OnInit, OnDestroy {
       }
     });
   }
-  onEditorCreated(editor) {
-    console.log("note",this.noteContent)
-    const savedNote = this.noteContent;
 
-    if (savedNote) {
-      editor.clipboard.dangerouslyPasteHTML(savedNote);
-    }
-  }
   onInput(): void {
     clearTimeout(this.timeoutId);
     this.timeoutId = setTimeout(() => {
-      debugger;
+      debugger
+      console.log('11111111',this.noteContent);
       if (this.isConnected) {
-        this.signalRService.sendMessage(this.noteContent, parseInt(this.fileId));
+        console.log('2222222',this.noteContent);
+        this.signalRService.sendMessage(this.noteContent,parseInt(this.fileId));
       } else {
         console.error('SignalR connection is not in the Connected state.');
+      
       }
     }, 300);
   }
-  
-  onNoteContentChange(event: any) {
-    // Lưu nội dung ghi chú vào localStorage
-    localStorage.setItem('savedNote', this.noteContent);
-     console.log('Note content saved:', this.noteContent);
-  }
+
   ngOnDestroy(): void {
     if (this.routeSub) {
       this.routeSub.unsubscribe();
     }
   }
+  
 }
