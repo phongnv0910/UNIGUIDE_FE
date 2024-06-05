c5fdb580ee31a155efdf85c4faf21f803bbf0617
tranhaaaaaa <cauchunso2002@gmail.com>
2024-06-05 10:33:03 +0700

    url product

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://studentguidebe.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://studentguidebe.azurewebsites.net/api'
+  apiUrl: 'https://localhost:7144/api'
 };
 
 /*
